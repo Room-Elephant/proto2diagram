c25bb97f773d1085aa59ab467d11f5140fc1ae80
JPedroBorges <JPedroBorges@users.noreply.github.com>
2025-08-02 20:54:40 +0100

    feat: add prettier and eslint config

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'no-console': 'warn',
+      'prettier/prettier': 'error',
+    },
+  },
+];
