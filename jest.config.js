9b8c5c2cb4cbfb637df34d092e14bce79c5023e7
JPedroBorges <JPedroBorges@users.noreply.github.com>
2025-08-04 01:01:51 +0100

    chore: add basic ci

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,12 @@ export default {
   },
   moduleFileExtensions: ["ts", "js"],
   testMatch: ["**/*.spec.js", "**/*.spec.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
